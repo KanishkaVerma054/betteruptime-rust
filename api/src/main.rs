01c1ba6db14a4cacd1501af56ea4c916cc5c8d79
Kanishka Verma <kanishkav774@gmail.com>
2025-09-13 17:01:58 +0530

    routes created

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,26 +3,53 @@ use poem::{
     get, handler, listener::TcpListener, post, web::{Json, Path}, Route, Server
 };
 
-use crate::{request_inputs::CreateWebsiteInput, request_outputs::CreateWebsiteOutput};
+use crate::{request_inputs::{CreateUserInput, CreateWebsiteInput}, request_outputs::{CreateUserOutput, CreateWebsiteOutput, GetWebsiteOutput, SigninOutput}};
 
-use store::Store;
+use store::{store::Store};
 
 pub mod request_inputs;
 pub mod request_outputs;
 
 // getting json inputs, giving json outputs
 #[handler]
-fn get_website(Path(name): Path<String>) -> String {
-    
-    format!("hello: {name}")
+fn get_website(Path(id): Path<String>) -> Json<GetWebsiteOutput> {
+    let mut s = Store::new().unwrap();
+    let website = s.get_website(id).unwrap();
+    Json(GetWebsiteOutput {
+        url: website.url
+    })
+}
+
+#[handler]
+fn sign_up(Json(data): Json<CreateUserInput>) -> Json<CreateUserOutput> {
+    let mut s = Store::new().unwrap();
+    let id = s.sign_up(data.username, data.password).unwrap();
+
+    let response = CreateUserOutput {
+        id
+    };
+
+    Json(response)
+}
+
+#[handler]
+fn sign_in(Json(data): Json<CreateUserInput>) -> Json<SigninOutput> {
+    let mut s = Store::new().unwrap();
+    let _exists = s.sign_in(data.username, data.password).unwrap();
+
+    let response = SigninOutput {
+        jwt: String::from("kanishk")
+    };
+
+    Json(response)
 }
 
 #[handler]
 fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
-    let s = Store{};
-    let id = s.create_website();
+    let mut s = Store::new().unwrap();
+    let website = s.create_website(String::from("a01b0fb8-4335-47e1-aee9-3f818553e4d8"), data.url).unwrap();
     let response = CreateWebsiteOutput {
-        id
+        id: website.id
     };
     // persist in the DB
     Json(response)
@@ -32,7 +59,9 @@ fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOut
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
         .at("/website/:website_id", get(get_website))
-        .at("/website", post(create_website));
+        .at("/website", post(create_website))
+        .at("/user/signup", post(sign_up))
+        .at("/user/signin", post(sign_in));
     // create and runs the http server
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .name("hello-world")
--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,24 +1,5 @@
 pub mod schema;
-pub struct Store {
-    conn: Connection
-}
+pub mod config;
+pub mod store;
+pub mod models;
 
-impl Default for Store {
-    fn default() -> Self {
-        
-        Self {
-            conn
-        }
-    } 
-}
-
-impl Store {
-    pub fn create_user(&self) {
-        print!("create user called");
-        self.conn.execute("INSERT INTO xyz")
-    }
-
-    pub fn create_website(&self) -> String {
-        String::from("1")
-    }
-}
